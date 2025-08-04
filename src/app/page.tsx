c2e3098b867b01c4dd74370d6a00b99b29e587fa
clevermiraz <mirazhs@pm.me>
2025-08-04 19:57:43 -0400

    05 AI Jobs Done 🪄

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function HomePage() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,7 +20,8 @@ export default function HomePage() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Jhon" })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
         Invoke Background Job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,19 @@
+import { createAgent, openai } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // Imagine this is a download step
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const agent = createAgent({
+      name: "agent",
+      system: "You are an expert teacher. whatever subject or topic user want you will give him.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await agent.run(`give details the following text: ${event.data.value}`);
+
+    return { output };
   }
 );
