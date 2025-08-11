83edaed9a785a00ac1ee297871e2cbe011a2301b
clevermiraz <mirazhs@pm.me>
2025-08-10 23:04:21 -0400

    06 E2B Sandboxes Done 🎉

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, openai } from "@inngest/agent-kit";
-
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-final-1459");
+
+      return sandbox.sandboxId;
+    });
+
     const agent = createAgent({
       name: "agent",
       system: "You are an expert teacher. whatever subject or topic user want you will give him.",
@@ -14,6 +21,14 @@ export const helloWorld = inngest.createFunction(
 
     const { output } = await agent.run(`give details the following text: ${event.data.value}`);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sendbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+
+      const host = sandbox.getHost(3000);
+
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
