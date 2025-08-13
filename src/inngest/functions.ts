e826f73c99551530696affc3ec3ce82428f11185
clevermiraz <mirazhs@pm.me>
2025-08-12 23:00:54 -0400

    Agent Tools Started.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 import { Sandbox } from "@e2b/code-interpreter";
-import { createAgent, openai } from "@inngest/agent-kit";
+import { createAgent, createTool, openai } from "@inngest/agent-kit";
+import z from "zod";
 import { inngest } from "./client";
 import { getSandbox } from "./utils";
 
@@ -13,13 +14,59 @@ export const helloWorld = inngest.createFunction(
       return sandbox.sandboxId;
     });
 
-    const agent = createAgent({
-      name: "agent",
-      system: "You are an expert teacher. whatever subject or topic user want you will give him.",
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
       model: openai({ model: "gpt-4o" }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data;
+                  },
+                });
+                return result.stdout;
+              } catch (error) {
+                console.error(
+                  `Command failed: ${error} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`
+                );
+                return `Command failed: ${error} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`;
+              }
+            });
+          },
+        }),
+
+        createTool({
+          name: "createOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            files: z.array(
+              z.object({
+                path: z.string(),
+                content: z.string(),
+              })
+            ),
+          }),
+        }),
+      ],
     });
 
-    const { output } = await agent.run(`give details the following text: ${event.data.value}`);
+    const { output } = await codeAgent.run(`give details the following text: ${event.data.value}`);
 
     const sandboxUrl = await step.run("get-sendbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
